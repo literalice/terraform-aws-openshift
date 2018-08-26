c8aecfdac39036725fb429007ddcd5b993df85f6
literalice <literalice@monochromeroad.com>
2018-08-27 05:32:03 +0900

    for ansible 2.4

--- resources/openshift-install.sh
+++ resources/openshift-install.sh
@@ -17,12 +17,11 @@ ocinventory -cluster "${platform_name}" -inventory $HOME/template-inventory.yml 
 export LANG=C
 
 if [ -z $UPSTREAM ]; then
-    ENABLE_REPOS="rhel-7-server-rpms,rhel-7-server-extras-rpms,rhel-7-fast-datapath-rpms,rhel-7-server-ansible-2.4-rpms"
-    ENABLE_REPOS="$ENABLE_REPOS,rhel-7-server-ose-${openshift_major_version}-rpms"
-
     ansible all -i $HOME/inventory.yml -m redhat_subscription -a 'username=${rhn_username} password=${rhn_password} pool_ids=${rh_subscription_pool_id}'
-    ansible all -i $HOME/inventory.yml -m rhsm_repository -a 'name="*" state=disabled'
-    ansible all -i $HOME/inventory.yml -m rhsm_repository -a "name=$ENABLE_REPOS"
+
+    ansible all -i $HOME/inventory.yml -a 'subscription-manager repos --disable="*"'
+    ansible all -i $HOME/inventory.yml -a 'subscription-manager repos --enable="rhel-7-server-rpms" --enable="rhel-7-server-extras-rpms" --enable="rhel-7-fast-datapath-rpms" --enable="rhel-7-server-ansible-2.4-rpms"'
+    ansible all -i $HOME/inventory.yml -a 'subscription-manager repos --enable="rhel-7-server-ose-${openshift_major_version}-rpms"'
 fi
 
 ansible-playbook -i $HOME/inventory.yml /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml -vvv | tee ansible-$(date +%Y%m%d%H%M%S)-prerequisites.log
