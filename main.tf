094fe621676f708495ac94101c50d2c6f5b52105
literalice <literalice@monochromeroad.com>
2018-11-25 13:42:15 +0900

    main module

--- main.tf
+++ main.tf
@@ -1,42 +1,54 @@
-module "openshift_network" {
+module "network" {
   source        = "modules/network"
   platform_name = "${var.platform_name}"
 }
 
-module "openshift_infra" {
+module "infra" {
   source = "modules/infra"
 
   platform_name = "${var.platform_name}"
+  use_community = "${var.use_community}"
 
-  platform_vpc_id    = "${var.platform_vpc_id}"
-  public_subnet_ids  = ["${var.public_subnet_ids}"]
-  private_subnet_ids = ["${var.private_subnet_ids}"]
+  platform_vpc_id    = "${module.network.platform_vpc_id}"
+  public_subnet_ids  = ["${module.public_subnet_ids}"]
+  private_subnet_ids = ["${module.private_subnet_ids}"]
 
-  operator_cidrs = ["0.0.0.0/0"]
+  operator_cidrs = ["${var.operator_cidrs}"]
+  public_cidrs   = ["${var.public_cidrs}"]
 
-  use_spot = true
+  use_spot = "${var.use_spot}"
 
-  master_count = "${var.master_count}"
+  master_count               = "${var.master_count}"
+  master_instance_type       = "${var.master_instance_type}"
+  compute_node_count         = "${var.compute_node_count}"
+  compute_node_instance_type = "${var.compute_node_instance_type}"
 }
 
-module "openshift_domain" {
-  source                              = "modules/domain"
+module "domain" {
+  source = "modules/domain"
+
   platform_name                       = "${var.platform_name}"
   platform_domain                     = "${var.platform_domain}"
   platform_domain_administrator_email = "${var.platform_domain_administrator_email}"
-  public_lb_arn                       = "${var.public_lb_arn}"
+  public_lb_arn                       = "${module.infra.public_lb_arn}"
 }
 
 module "openshift" {
   source = "modules/openshift"
 
-  platform_name           = "${var.platform_name}"
-  bastion_ssh_user        = "${var.bastion_ssh_user}"
-  bastion_endpoint        = "${var.bastion_endpoint}"
-  platform_private_key    = "${var.platform_private_key}"
+  platform_name = "${var.platform_name}"
+  use_community = "${var.use_community}"
+
+  bastion_ssh_user        = "${module.infra.bastion_ssh_user}"
+  bastion_endpoint        = "${module.infra.bastion_endpoint}"
+  platform_private_key    = "${module.infra.platform_private_key}"
   rhn_username            = "${var.rhn_username}"
   rhn_password            = "${var.rhn_password}"
   rh_subscription_pool_id = "${var.rh_subscription_pool_id}"
-  master_domain           = "${var.master_domain}"
-  platform_domain         = "${var.platform_domain}"
+
+  master_domain                       = "${module.infra.master_domain}"
+  platform_domain                     = "${var.platform_domain}"
+  public_certificate_pem              = "${module.domain.public_certificate_pem}"
+  public_certificate_key              = "${module.domain.public_certificate_key}"
+  public_certificate_intermediate_pem = "${module.domain.public_certificate_intermediate_pem}"
 }
--- modules/infra/output.tf
+++ modules/infra/output.tf
@@ -10,3 +10,11 @@ output "platform_private_key" {
   sensitive = true
   value     = "${data.tls_public_key.platform.private_key_pem}"
 }
+
+output "public_lb_arn" {
+  value = ["${aws_lb.public.arn}"]
+}
+
+output "master_domain" {
+  value = "${aws_route53_record.master.name}"
+}
